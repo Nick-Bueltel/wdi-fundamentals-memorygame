1d0fea6a7c419c25c61e10c332f4b878b1099a33
Nick-Bueltel <nickbueltel@gmail.com>
2019-04-19 21:26:29 -0500

    updated organization

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,15 +2,25 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay =[];
 
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2){
+function checkForMatch(){
     if(cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else{ alert("sorry try again.")}
+
 }
+
+function flipCard(cardId){
+    var cardOne = cards[0];
+    cardsInPlay.push(cardOne);
+
+    var cardTwo = cards[2];
+    cardsInPlay.push(cardTwo);
+
+    if(cardsInPlay.length === 2){
+        checkForMatch();
+
+    }
+
+
+}
+
